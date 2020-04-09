c9497dc70681c3064ea4ca0f9fe6c4b8e00e3864
coder&coding <38404887+paranjoy@users.noreply.github.com>
2020-04-09 19:50:37 +0530

    question 4

---
+++ turn_off_ith_bit.cpp
@@ -0,0 +1,32 @@
+/*
+You are given an integer Ni. You need to make first set bit of binary representation of N to 0 and return the updated N.
+Counting of bits start from 0 from right to left.
+
+Input Format :
+Integer N 
+
+Output Format :
+Updated N
+
+Sample Input 1 :
+4
+Sample Output 1 :
+0
+
+Sample Input 2 :
+12 
+Sample Output 2 :
+8
+*/
+//code snippet
+
+int turnOffIthBit(int n, int i){
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+    return ( n & ~(1<<i)); 
+}
+
+
